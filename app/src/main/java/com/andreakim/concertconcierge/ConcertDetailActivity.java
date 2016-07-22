9444673c04343e5b32de0f4317d9e88ff5ca10b7
Andrea Kim <andreaforanimals@gmail.com>
2016-07-22 14:46:16 -0500

    created file

---
+++ app/src/main/java/com/andreakim/concertconcierge/ConcertDetailActivity.java
@@ -0,0 +1,13 @@
+package com.andreakim.concertconcierge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConcertDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_concert_detail);
+    }
+}
